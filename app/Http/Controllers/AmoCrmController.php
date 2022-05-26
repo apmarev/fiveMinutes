918b8ca11c258b1733d657b66fb16e07c311aed3
apmarev <57302841+apmarev@users.noreply.github.com>
2022-05-26 12:58:50 +0200

    initial

--- app/Http/Controllers/AmoCrmController.php
+++ app/Http/Controllers/AmoCrmController.php
@@ -62,7 +62,7 @@ class AmoCrmController extends Controller {
 
             Telegram::sendMessage([
                 'chat_id' => '228519769',
-                'text' => "Изменение: {$talkId}"
+                'text' => json_encode($request->input('talk'))
             ]);
 
             if($message = Message::where('chatId', $chatId)->first()) {
