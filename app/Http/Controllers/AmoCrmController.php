77112359e79ee13cd6c4ca64de34703b39fc95a9
apmarev <57302841+apmarev@users.noreply.github.com>
2022-05-30 19:14:04 +0200

    initial

--- app/Http/Controllers/AmoCrmController.php
+++ app/Http/Controllers/AmoCrmController.php
@@ -1335,7 +1335,7 @@ class AmoCrmController extends Controller {
     }
 
     public function pactNewMessage(Request $request) {
-        if($request->has('data')) {
+        if($request->has('event') && $request->input('event') == 'new') {
             Telegram::sendMessage([
                 'chat_id' => '228519769',
                 'text' => json_encode($request->input('data'))
