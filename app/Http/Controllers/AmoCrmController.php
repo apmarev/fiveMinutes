48533d8a06efc32c4c8632ab5c90210ae577d653
apmarev <57302841+apmarev@users.noreply.github.com>
2022-05-31 16:13:23 +0200

    initial

--- app/Http/Controllers/AmoCrmController.php
+++ app/Http/Controllers/AmoCrmController.php
@@ -1362,6 +1362,12 @@ class AmoCrmController extends Controller {
         if($request->has('event') && $request->input('event') == 'new' && $request->has('type') && $request->input('type') == 'message') {
             $message = $request->input('data');
 
+            if(isset($message['income']))
+                Telegram::sendMessage([
+                    'chat_id' => '228519769',
+                    'text' => $message['income'] > 0 ? "Большле = {$message['income']}" : "Меньше = {$message['income']}"
+                ]);
+
             if(
                 isset($message['income']) &&
                 $message['income'] == 0 &&
